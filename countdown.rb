ea63bc2e0f6c62b96da88f2491cde28adf580039
Artiium Arbyummi <artiium21@gmail.com>
2019-10-12 03:11:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(new_years)
   countdown = 10 
   while countdown > 0
-  puts "#{countdown} SECONDS (S)!"
+  puts "#{countdown} SECONDS(S)!"
   countdown -= 1
   return "HAPPY NEW YEAR!"
 end
